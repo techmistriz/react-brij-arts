ee283a209f0ce8757eb3c7a8f79a5db709bf5d80
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-03-13 16:32:00 +0530

    attach token automatically configuration in axios setup

---
+++ src/lib/axios.ts
@@ -0,0 +1,25 @@
+// src/lib/axios.ts
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// attach token automatically
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
