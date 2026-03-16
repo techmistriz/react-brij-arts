f9679ee824ae3596fd573ba6ceea3fd2c1c91d7b
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-03-16 14:57:51 +0530

    axios setup and contact api fetch

---
+++ src/lib/api/contact.ts
@@ -0,0 +1,17 @@
+import axiosInstance from "../axios";
+
+export interface ContactFormData {
+  name: string;
+  email: string;
+  contact: string;
+}
+
+export const submitContactForm = async (data: ContactFormData) => {
+  try {
+    const response = await axiosInstance.post("/contact-us-lead", data);
+    return response.data;
+  } catch (error: any) {
+    console.log(error.response?.data);
+    throw error;
+  }
+};
